224d1808758df004da74afaa14d288dbf3025677
Vikram Sai Simha Vissamsetti <vikramvissamsetti@gmail.com>
2026-07-23 12:13:44 +0530

    Point cask URLs at renamed repo paramzvik/quicknib

--- Casks/quicknib.rb
+++ Casks/quicknib.rb
@@ -2,10 +2,10 @@ cask "quicknib" do
   version "1.0.3"
   sha256 "6015b752566de8fa1c2a7c0c2969b3f9312be4cbda3c66ccd5c45494f4d244b2"
 
-  url "https://github.com/paramzvik/notepad-macos/releases/download/v#{version}/QuickNib-#{version}-arm64.dmg"
+  url "https://github.com/paramzvik/quicknib/releases/download/v#{version}/QuickNib-#{version}-arm64.dmg"
   name "QuickNib"
   desc "Lightweight notepad with syntax highlighting"
-  homepage "https://github.com/paramzvik/notepad-macos"
+  homepage "https://github.com/paramzvik/quicknib"
 
   depends_on arch: :arm64
   depends_on macos: :big_sur
